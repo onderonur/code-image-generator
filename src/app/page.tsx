b476a4995a94abd8470f2b8d2fd7f018de2047c4
onderonur <onuronder.onr@gmail.com>
2023-02-12 02:03:55 +0300

    added todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Layout from '@/layout/Layout';
 import Hero from '@/hero/Hero';
 import ExportableEditor from '@/exportable-editor/ExportableEditor';
 
+// TODO: License ekle educational purpose vs.
+
 export default function Page() {
   return (
     <Layout>
