8e6a2d0f537854a02780f95c1628e5cd81de50f0
onderonur <onuronder.onr@gmail.com>
2023-02-21 20:11:08 +0300

    package updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Layout from '@/layout/Layout';
 import Hero from '@/hero/Hero';
 import ExportableEditor from '@/exportable-editor/ExportableEditor';
 
-// TODO: License ekle educational purpose vs.
-
 export default function Page() {
   return (
     <Layout>
