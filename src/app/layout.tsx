78e10488909bf23b4a9224c5e8a41e53cb31d0e0
onderonur <onuronder.onr@gmail.com>
2023-02-26 19:19:14 +0300

    next 13.2 migration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,54 @@
 import { Inter } from '@next/font/google';
 import '@/styling/globals.css';
+import { Metadata } from 'next';
+import { APP_TITLE } from '@/common/CommonUtils';
 
 const font = Inter({ subsets: ['latin'] });
+
+export function generateMetadata(): Metadata {
+  const metaTitle = APP_TITLE;
+  const metaDescription = `Create code images with ease by using ${APP_TITLE}`;
+  const images = [
+    {
+      url: '/og-image.jpg',
+      alt: `${metaTitle} Logo`,
+      width: 1200,
+      height: 630,
+    },
+  ];
+
+  return {
+    title: metaTitle,
+    description: metaDescription,
+    themeColor: '#0f172a',
+    creator: 'Onur Önder',
+    metadataBase: new URL(`${process.env.BASE_URL}${process.env.BASE_PATH}`),
+    verification: {
+      google: process.env.GOOGLE_SITE_VERIFICATION,
+    },
+    openGraph: {
+      type: 'website',
+      title: metaTitle,
+      description: metaDescription,
+      siteName: APP_TITLE,
+      locale: 'en_US',
+      images,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: metaTitle,
+      description: metaDescription,
+      images,
+      creator: '@onderonur_',
+    },
+  };
+}
 
 type RootLayoutProps = React.PropsWithChildren;
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={font.className}>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body className="bg-body-900 text-white">{children}</body>
     </html>
