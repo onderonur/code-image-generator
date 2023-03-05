3de4fa383204eb82b2ba55656959543deeff8bb7
Onur Önder <50423574+onderonur@users.noreply.github.com>
2023-03-05 15:56:28 +0300

    Create nextjs.yml

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-body-900 text-white font-sans">
+      <body className="bg-body-900 text-white">
         <Main />
         <NextScript />
       </body>
