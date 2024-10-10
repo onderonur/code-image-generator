75364c2a944567575b81cb4cbe501a2f258316fc
onderonur <onuronder.onr@gmail.com>
2024-10-10 22:07:03 +0300

    improved next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     // To make `next lint` check files and folders besides the default folders (`src`, `app` etc.):
     // https://nextjs.org/docs/app/building-your-application/configuring/eslint#linting-custom-directories-and-files
